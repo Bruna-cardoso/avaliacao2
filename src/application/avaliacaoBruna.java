5102b2a9f584eb0c7a35c1c47f370750c8f79b71
Bruna Cardoso <bruna.carolinepc@gmail.com>
2020-05-29 19:23:22 -0300

    Leitura dos dados

--- src/application/avaliacaoBruna.java
+++ src/application/avaliacaoBruna.java
@@ -9,6 +9,50 @@ public class avaliacaoBruna {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		String nome;
+		int horasTrabalhadas;
+		double valorHora;
+		char continuar;
+		
+		// Leitura dos dados
+		
+		
+		System.out.print("Nome: ");
+		nome = sc.next();
+		System.out.print("Horas trabalhadas: ");
+		horasTrabalhadas = sc.nextInt();
+			while (horasTrabalhadas < 0) {
+				System.out.println("Valor inválido! Por favor digite novamente.");
+				System.out.print("Horas trabalhadas: ");
+				horasTrabalhadas = sc.nextInt();
+			}
+		System.out.print("Valor por hora: ");
+		valorHora = sc.nextDouble();
+			while (valorHora < 0) {
+				System.out.println("Valor inválido! Por favor digite novamente.");
+				System.out.print("Valor por hora: ");
+				valorHora = sc.nextDouble();
+			}
+		System.out.print("Digitar outro (S/N)? ");
+		continuar = sc.next().charAt(0);
+		
+		while (continuar == 'S' || continuar == 's') {
+			System.out.print("Nome: ");
+			nome = sc.next();
+			System.out.print("Horas trabalhadas: ");
+			horasTrabalhadas = sc.nextInt();
+			System.out.print("Valor por hora: ");
+			valorHora = sc.nextDouble();
+			System.out.print("Digitar outro (S/N)? ");
+			continuar = sc.next().charAt(0);
+		}
+		
+		
+		
+		
+		
+		sc.close();
 
 	}
 
